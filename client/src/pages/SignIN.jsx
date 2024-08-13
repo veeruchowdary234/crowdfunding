0b05f6ddf43a88b7f673748c40a7c776e4200ab6
veeru <veerumedikonduru999@gmail.com>
2024-08-13 13:00:47 +0530

    new

--- client/src/pages/SignIN.jsx
+++ client/src/pages/SignIN.jsx
@@ -24,7 +24,7 @@ function Signin()
     }
     try {
       dispatch(signin())
-      const res=await fetch('/api/auth/signin',
+      const res=await fetch('https://crowdfunding-31jm.onrender.com/api/auth/signin',
       {
         method:'POST',
         headers:{'Content-Type':'application/json'},
